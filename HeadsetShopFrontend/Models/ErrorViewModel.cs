48dd1f9007fad60802bee675f2783a57b5d31f2c
Steven <e0390018@u.nus.edu>
2025-04-24 00:41:21 +0800

    add main program

---
+++ HeadsetShopFrontend/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace HeadsetShopFrontend.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
